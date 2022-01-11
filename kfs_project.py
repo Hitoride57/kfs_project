1f5478c11f91afc7b99b4c458b9fe1600e1b77b2
Simon TOURDOT <69247004+Hitoride57@users.noreply.github.com>
2022-01-11 21:07:29 +0100

    ADD (kfs_project.py)
    Adding main file (for now)

---
+++ kfs_project.py
@@ -0,0 +1,9 @@
+print("\n*******************************")
+print("*** Kendo's Fight Simulator ***")
+print("******** By Hitoride57 ********")
+print("*******************************\n")
+
+    # Create fighter object to add color, grade and other informations
+input(fighter1:="\nEnter first kenshi's name : ")
+
+input(fighter2:="\nEnter second kenshi's name : ")
