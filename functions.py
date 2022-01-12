5a7d39c615eacf85f2cb7dc916c9e61a3ba9192a
Saimonn <simon.tourdot@linuxmail.org>
2022-01-12 23:53:20 +0100

    ADD (functions)
    Define kamae's choice function

---
+++ functions.py
@@ -0,0 +1,62 @@
+# Choose kamae function (ito, nito, jodan,...)
+
+
+def choose_kamae(fighter_to_affect=""):                     # Optional argument as long as there's no fighter object
+
+    while True:
+
+        kamae_set = input("Set your kamae (h for help) : ")
+
+        if kamae_set == "h":
+            print("""
+    Kamae's list :
+
+        - Chudan
+        - Hidari-Jodan
+        - Migi-Jodan
+        - Sai-Nito
+        - Gyaku-Nito
+
+    (Enter \"q\" at any moment to leave the simulator)
+                """)
+
+        elif kamae_set == "q":
+            print("\nLeaving the simulator\n")
+            break
+
+        elif kamae_set == "Chudan":
+            print("Ito kamae choosed")
+            return kamae_set
+            break
+
+        elif kamae_set == "Hidari-Jodan":
+            print("Hidari-Jodan kamae choosed")
+            return kamae_set
+            break
+
+        elif kamae_set == "Migi-Jodan":
+            print("Migi-Jodan kamae choosed")
+            return kamae_set
+            break
+
+        elif kamae_set == "Sai-Nito":
+            print("Sai-Nito kamae choosed")
+            return kamae_set
+            break
+
+        elif kamae_set == "Gyaku-Nito":
+            print("Gyaku-Nito kamae choosed")
+            return kamae_set
+            break
+
+        elif kamae_set == "Waki":
+            print("Waki kamae choosed (why not)")
+            return kamae_set
+            break
+
+kamae = choose_kamae()
+
+if not kamae:
+    print("No kamae set")
+else:
+    print("The kamae set is : " + kamae)
