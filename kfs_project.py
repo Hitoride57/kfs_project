7901c8d9b47c323a23f32c9ca94b508191ae0e44
Saimonn <simon.tourdot@linuxmail.org>
2022-01-24 23:47:05 +0100

    EDIT (kfs_project.py)
    Only 1 fighter to set (2 players mode will be developped later...much more later !)

--- kfs_project.py
+++ kfs_project.py
@@ -4,6 +4,4 @@ print("******** By Hitoride57 ********")
 print("*******************************\n")
 
     # Create fighter object to add color, grade and other informations
-input(fighter1:="\nEnter first kenshi's name : ")
-
-input(fighter2:="\nEnter second kenshi's name : ")
+input(fighter1:="\nEnter first kenshi name : ")
