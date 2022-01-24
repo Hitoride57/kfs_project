66ee9e4116712047b04a49f40036add2305d46da
Saimonn <simon.tourdot@linuxmail.org>
2022-01-24 23:48:01 +0100

    ADD (fighter_save.py)
    Function to save fighter for further combats

---
+++ functions/fighter_save.py
@@ -0,0 +1,12 @@
+    # Saving fighter info for further fights
+
+    # File will be name with fighter's name
+fighter_name = "Hito"
+
+save_fighter = open(fighter_name + ".sav", "w")
+
+"""
+    To be completed later
+"""
+
+save_fighter.close()
