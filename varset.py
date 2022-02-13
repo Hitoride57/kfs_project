c8e927a49c83f1a2a92b715fa42c8a8ebede0e73
Saimonn <simon.tourdot@linuxmail.org>
2022-02-13 22:37:49 +0100

    ADD (varset.py)
    Define variables preset (tuples)

---
+++ varset.py
@@ -0,0 +1,27 @@
+    # File for variables preset definition
+
+grade = list(range(0,9))
+
+competition = (
+    "excellence",
+    "honor",
+    "feminine",
+    "junior",
+    "kuysha"
+)
+
+kamae = (
+    "chudan",
+    "hidari-jodan",
+    "migi-jodan",
+    "sai-nito",
+    "gyaku-nito",
+    "waki"
+)
+
+target = (
+    "men",
+    "kote",
+    "do",
+    "tsuki"
+)
